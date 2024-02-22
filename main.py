0c3f9f216acf9f417a2e9480120be1cf5d3e1dc6
mr pakrouh <61857525+PYRONstar@users.noreply.github.com>
2024-02-22 19:36:58 +0330

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+import arabic_reshaper
+from bidi.algorithm import get_display
+
+api = "https://one-api.ir/joke/?token=307524:6263366ea74175.45250586" 
+
+
+def convert(text):
+    resharped_text=arabic_reshaper.reshape(text)
+    converted =get_display(resharped_text)
+    return converted
+
+
+yourapi = requests.get(api)
+valueJson = yourapi.json()["result"]
+
+print(convert(valueJson))
